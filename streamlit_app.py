41db905696d60cfab1907d3008d9a76358f2cb13
Adam <91267565+boud96@users.noreply.github.com>
2026-04-16 17:48:26 +0200

    Update and rename streamlit_app to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 st.title("Smoothies")
 
